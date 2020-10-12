c7fba3c20605580e5ffc2bf581abc170c9c5e78f
ha9011 <gkehddnjs90@gmail.com>
2020-10-12 23:49:28 +0900

    list item make

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
-const VideoItem = ({videos}) => {
+import styles from './video_item.module.css';
+const VideoItem = ({videos : {snippet}}) => {
     
-    console.log(videos);
+   
     return(
-    <div>{videos.snippet.title}</div>
+
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img className={styles.thumbnails} src={snippet.thumbnails.medium.url} alt="video thumbnail"/>
+            <div>
+                <p className={styles.title}>{snippet.title}</p>
+                <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div>
+
+        </div>
+      
+    </li>
+    
     );
 };
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 const VideoList = ({videos}) => {
 
@@ -7,9 +8,10 @@ const VideoList = ({videos}) => {
 
     return(
         
-            <ul>
-                {videos.map( video => (<VideoItem key={video["id"]} videos={video}/>))}
+            <ul className={styles.videos}>
+                    {videos.map( video => (<VideoItem  key={video["id"]} videos={video}/>))}    
             </ul>
+       
         
         
 
